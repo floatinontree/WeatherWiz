de065d4554d29edbb3c919cbb4425f6933b5fdfc
Yazan Eldisi <yazandisi@Yazans-MBP.home>
2023-02-13 19:29:04 -0600

    Update App.js and add more images

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ import picHot2 from "./assets/ResA4.png"
 import picCold0 from "./assets/ResA2.png"
 import picCold1 from "./assets/ResA5.png"
 import picCold2 from "./assets/ResA6.png"
-
+import picCloud0 from "./assets/ResA7.png"
+import picRain0 from "./assets/ResA8.png"
+import picClear0 from "./assets/ResA9.png"
+import picSnow0 from "./assets/ResB.png"
 countries.registerLocale(require("i18n-iso-countries/langs/en.json"));
 
 function App() {
@@ -29,15 +32,20 @@ function App() {
     }
   };
 
-  let parser = () => {
-    const weather = apiValue.weather[0].main;
-    console.log('parser', value, weather)
-  }
+  // let parser = () => {
+  //   const weather = apiValue.weather[0].main;
+  //   console.log('parser', value, weather)
+  // }
 
   useEffect(() => {
-    fetch(apiUrl)
-      .then((res) => res.json())
-      .then((data) => setApiValue(data));
+    if (state && getState) {
+      fetch(apiUrl)
+        .then((res) => res.json())
+        .then((data) => setApiValue(data)); fetch(apiUrl)
+          .then((res) => res.json())
+          .then((data) => setApiValue(data));
+    }
+
   }, [state, apiUrl]);
 
   const kelvToFarenheit = (k) => {
@@ -49,15 +57,17 @@ function App() {
 
   const handleSubmit = () => {
     setState(getState);
-    parser()
+    // parser()
   };
 
 
-console.log(apiValue)
+  // console.log(apiValue)
 
 
   return (
     <div className="App">
+
+
       <header className="d-flex justify-content-center align-items-center">
         <h2>Weather Wiz</h2>
       </header>
@@ -120,44 +130,107 @@ console.log(apiValue)
               className="form-control"
               onChange={takeInput}
               value={getState}
+              required
             />
           </div>
           <button className="btn btn-primary mt-2" onClick={handleSubmit}>
             Search
           </button>
         </div>
-
+        {apiValue.main && apiValue.main.temp ? (
         <div className="card mt-3 mx-auto" style={{ width: "60vw" }}>
 
-        {/* Josh Work TODO *******************************************************************/}
+          {/* Josh Work TODO *******************************************************************/}
           <div>
-            {value === "Res-A" && kelvToFarenheit(apiValue.main.temp) > 45 ? 
-            <div><img src={picHot0} /> <img src={picHot1} /><img src={picHot2} /> </div> 
-            : null}
+            {value === "Res-A" && kelvToFarenheit(apiValue.main.temp) > 45 ?
+              <div><img src={picHot0} /> <img src={picHot1} /><img src={picHot2} /> </div>
+              : null}
 
-            {value === "Res-A" && kelvToFarenheit(apiValue.main.temp) < 45 ? 
-            <div><img src={picCold0} /> <img src={picCold1} /><img src={picCold2} /></div> 
-            : null}
+            {value === "Res-A" && kelvToFarenheit(apiValue.main.temp) < 45 ?
+              <div><img src={picCold0} /> <img src={picCold1} /><img src={picCold2} /></div>
+              : null}
 
-          {value === "Res-B" && kelvToFarenheit(apiValue.main.temp) > 45 ? 
-            <div><img src={picHot0} /> <img src={picHot1} />  <p className="h2">{kelvToFarenheit(apiValue.main.temp)}° F</p></div> 
-            : null}
+            {value === "Res-B" && kelvToFarenheit(apiValue.main.temp) > 45 ?
+              <div>
+                <img src={picHot0} />
+                <img src={picHot1} />
+                <p className="h2">{kelvToFarenheit(apiValue.main.temp)}° F</p>
+                <p><strong>Sky: </strong></p>
+                {apiValue.weather[0].main === "Clouds" && <h2><img src={picCloud0} /></h2>}
+                {apiValue.weather[0].main === "Rain" && <h2><img src={picRain0} /></h2>}
+                {apiValue.weather[0].main === "Clear" && <h2><img src={picClear0} /></h2>}
+                {apiValue.weather[0].main === "Snow" && <h2><img src={picSnow0} /></h2>}
+              </div>
+              : null}
 
-          {value === "Res-B" && kelvToFarenheit(apiValue.main.temp) < 45 ? 
-            <div><img src={picCold0} /> <img src={picCold1} />  <p className="h2">{kelvToFarenheit(apiValue.main.temp)}° F</p></div> 
-            : null}
+            {value === "Res-B" && kelvToFarenheit(apiValue.main.temp) < 45 ?
+              <div><img src={picCold0} /> <img src={picCold1} />  <p className="h2">{kelvToFarenheit(apiValue.main.temp)}° F</p><p><strong>Sky:</strong></p>
+                {apiValue.weather[0].main === "Clouds" && <h2><img src={picCloud0} /></h2>}
+                {apiValue.weather[0].main === "Rain" && <h2><img src={picRain0} /></h2>}
+                {apiValue.weather[0].main === "Clear" && <h2><img src={picClear0} /></h2>}
+                {apiValue.weather[0].main === "Snow" && <h2><img src={picSnow0} /></h2>}
 
+              </div>
+              : null}
 
+            {value === "Res-C" && kelvToFarenheit(apiValue.main.temp) > 45 ?
+              <div><img src={picHot0} />  <p className="h2">{kelvToFarenheit(apiValue.main.temp)}° F</p><p><strong>Sky: {apiValue.weather[0].main}</strong></p>
+                {apiValue.weather[0].main === "Clouds" && <h2><img src={picCloud0} /></h2>}
+                {apiValue.weather[0].main === "Rain" && <h2><img src={picRain0} /></h2>}
+                {apiValue.weather[0].main === "Clear" && <h2><img src={picClear0} /></h2>}
+                {apiValue.weather[0].main === "Snow" && <h2><img src={picSnow0} /></h2>}</div>
+              : null}
+
+            {value === "Res-C" && kelvToFarenheit(apiValue.main.temp) < 45 ?
+              <div><img src={picCold0} />  <p className="h2">{kelvToFarenheit(apiValue.main.temp)}° F</p><p><strong>Sky: {apiValue.weather[0].main}</strong></p>
+                {apiValue.weather[0].main === "Clouds" && <h2><img src={picCloud0} /></h2>}
+                {apiValue.weather[0].main === "Rain" && <h2><img src={picRain0} /></h2>}
+                {apiValue.weather[0].main === "Clear" && <h2><img src={picClear0} /></h2>}
+                {apiValue.weather[0].main === "Snow" && <h2><img src={picSnow0} /></h2>}
+              </div>
+              : null}
+
+            {value === "Res-D" && kelvToFarenheit(apiValue.main.temp) > 45 ?
+              <div><p className="h2">{kelvToFarenheit(apiValue.main.temp)}° F</p><p className="h2">Feels Like {kelvToFarenheit(apiValue.main.feels_like)}° F</p><p>Atmosphere: {apiValue.weather[0].main}</p>
+               {apiValue.weather[0].main === "Clouds" && <h2><img src={picCloud0} /></h2>}
+                {apiValue.weather[0].main === "Rain" && <h2><img src={picRain0} /></h2>}
+                {apiValue.weather[0].main === "Clear" && <h2><img src={picClear0} /></h2>}
+                {apiValue.weather[0].main === "Snow" && <h2><img src={picSnow0} /></h2>}
+              
+              <p>Cloudy weather is when the sky is covered with clouds, which are made up of tiny water droplets or ice crystals. Clouds form when warm air rises and cools, causing the water vapor in the air to condense into tiny droplets or ice crystals.
+
+                The amount and type of clouds can help us predict the weather. For example, white, puffy clouds usually mean fair weather, while dark, flat clouds can indicate a storm is coming.
+
+                Different heights and thicknesses of clouds can also give us information about the weather. Low clouds, like stratus clouds, often bring drizzle or light rain. Middle clouds, like altocumulus clouds, can indicate a change in weather, like a storm coming or going. High clouds, like cirrus clouds, are often a sign of fair weather.
+
+                In general, cloudy weather can mean that it is cooler and there is less sunshine, so it's a good idea to dress in layers to stay warm and comfortable.</p></div>
+              : null}
+
+            {value === "Res-D" && kelvToFarenheit(apiValue.main.temp) < 45 ?
+              <div>  <p className="h2">{kelvToFarenheit(apiValue.main.temp)}° F</p><p className="h2">Feels Like {kelvToFarenheit(apiValue.main.feels_like)}° F</p><p>Atmosphere: {apiValue.weather[0].main}</p>
+              
+               {apiValue.weather[0].main === "Clouds" && <h2><img src={picCloud0} /></h2>}
+                {apiValue.weather[0].main === "Rain" && <h2><img src={picRain0} /></h2>}
+                {apiValue.weather[0].main === "Clear" && <h2><img src={picClear0} /></h2>}
+                {apiValue.weather[0].main === "Snow" && <h2><img src={picSnow0} /></h2>}
+              
+              
+              <p>Cloudy weather is when the sky is covered with clouds, which are made up of tiny water droplets or ice crystals. Clouds form when warm air rises and cools, causing the water vapor in the air to condense into tiny droplets or ice crystals.
+
+                The amount and type of clouds can help us predict the weather. For example, white, puffy clouds usually mean fair weather, while dark, flat clouds can indicate a storm is coming.
+
+                Different heights and thicknesses of clouds can also give us information about the weather. Low clouds, like stratus clouds, often bring drizzle or light rain. Middle clouds, like altocumulus clouds, can indicate a change in weather, like a storm coming or going. High clouds, like cirrus clouds, are often a sign of fair weather.
+
+                In general, cloudy weather can mean that it is cooler and there is less sunshine, so it's a good idea to dress in layers to stay warm and comfortable.</p></div>
+              : null}
 
           </div>
-      {/* Josh Work TODO ***********************************************************************/}
-        <h1>-----------------------</h1>
-          {apiValue.main ? (
+          {/* Josh Work TODO ***********************************************************************/}
+          {apiValue.main && state ? (
             <div className="card-body text-center">
-              <h1 className="mt-3" >{value}</h1>
 
 
-              <p className="h2">{kelvToFarenheit(apiValue.main.temp)}° F</p>
+
 
               <p className="h5">
                 <i className="fas fa-map-marker-alt"></i>{" "}
@@ -167,44 +240,17 @@ console.log(apiValue)
                 })}
               </p>
 
-              <div className="row mt-4">
-                <div className="col-md-6">
-                  <p>
-                    <i className="fas fa-temperature-low "></i>{" "}
-                    <strong>
-                      {kelvToFarenheit(apiValue.main.temp_min)}&deg; F
-                    </strong>
-                  </p>
-                  <p>
-                    <i className="fas fa-temperature-high"></i>{" "}
-                    <strong>
-                      {kelvToFarenheit(apiValue.main.temp_max)}&deg; F
-                    </strong>
-                  </p>
-                </div>
-                <div className="col-md-6">
-                  <p>
-                    <strong>Sky: {apiValue.weather[0].main} </strong>
-                  </p>
-                </div>
-              </div>
             </div>
           ) : (
             <h1>Please Enter A Valid City</h1>
           )}
         </div>
+        ) : <h1 className="card-body text-center mt-4">Please Enter A Valid City</h1>}
       </div>
-
-      <footer className="footer">Footer area</footer>
+      
     </div>
+    
   );
 }
 
 export default App;
-
-
-           {/* <img
-                src={`http://openweathermap.org/img/w/${apiValue.weather[0].icon}.png`}
-                alt="weather status icon"
-                className="weather-icon"
-              /> */}
